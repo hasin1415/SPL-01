8f03ed11e920993a36e1a1e68beb98e567029d85
Hasin Ahmed <bsse1415@iit.du.ac.bd>
2023-09-10 09:44:00 +0600

    file readesr

--- MatrixManiPulator.h
+++ MatrixManiPulator.h
@@ -7,12 +7,38 @@
 
 #include "vector"
 class MatrixManiPulator {
-public :
-    vector<vector<double>>MatrixMultiplicator(){
+    vector<vector<double>> matrix_multiplication(vector<vector<double>> A, vector<vector<double>> B) {
+        // Check if the matrices can be multiplied.
+        int m = A.size();
+        int n = A[0].size();
+        int p = B[0].size();
+        if (n != B.size()) {
+            cout << "invalid size";
+        }
 
+        vector<vector<double>> C(m, vector<double>(p));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < p; j++) {
+                for (int k = 0; k < n; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
 
-}
+        return C;
+    }
+    vector<vector<double>> matrix_transpose(vector<vector<double>> A) {
+        int m = A.size();
+        int n = A[0].size();
+        vector<vector<double>> T(n, vector<double>(m));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                T[j][i] = A[i][j];
+            }
+        }
 
+        return T;
+    }
 };
 
 
